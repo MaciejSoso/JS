c9fbb0eef4b11cb30f09b6433152990f7ad29749
MaciejSoso <51401384+MaciejSoso@users.noreply.github.com>
2019-07-19 20:03:45 +0200

    Add files via upload

---
+++ 22/js/main.js
@@ -0,0 +1,7 @@
+let number = 12;
+console.log(number)
+if(number> 10) {
+    console.log('Podana liczba jest większa od 10')
+}else if(number<10){
+    console.log('Podana liczba nie jest większa od 10')
+}
