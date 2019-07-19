8a81b018fd06297b4384df59e14c326a09842afd
MaciejSoso <51401384+MaciejSoso@users.noreply.github.com>
2019-07-19 19:40:15 +0200

    Add files via upload

---
+++ zmien/js/main.js
@@ -0,0 +1,13 @@
+let number1 = 112
+console.log(number1);
+let number2 = 997
+console.log(number2);
+let number3 = 998
+console.log(number3)
+let array = [number1, number3, number2];
+console.log(array); 
+
+
+
+
+
